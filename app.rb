41eef46ff85e250b016b2e1582a4a57c02db4706
Shintani Teppei <teppest1215@gmail.com>
2017-07-09 10:59:15 +0900

    bootstrapを追加して適当なフォームを作った

--- app.rb
+++ app.rb
@@ -6,6 +6,14 @@ Dotenv.load
 load './TwitterClient.rb'
 
 
+get '/' do
+  erb :index
+end
+
+get '/request' do
+	params["name"]
+end
+
 get "/tweets/:user_name" do
   tc = TwitterClient.new
   user = tc.show_user_timeline(params[:user_name])
